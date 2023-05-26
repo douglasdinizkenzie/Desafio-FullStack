9593f385494d49681e53882e43fe9e931355936b
Douglas <douglasslycooper@gmail.com>
2023-05-25 22:03:34 -0300

    feat: add cors to the project

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import "express-async-errors";
+import cors from "cors";
 import express, { Application } from "express";
 import { handleErrors } from "./errors";
 import { usersRoutes } from "./routes/users.routes";
@@ -7,6 +8,7 @@ import { loginRoutes } from "./routes/login.routes";
 import { contactsRoutes } from "./routes/contacts.routes";
 
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/users", usersRoutes);
